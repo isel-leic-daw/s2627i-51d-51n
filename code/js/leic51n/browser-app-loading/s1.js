c29a490366a3b40721c55eb82f676251dd3c8981
Pedro Felix <pmhsfelix@gmail.com>
2026-09-13 11:11:51 +0100

    adds browser-app-loading example for leic51n

---
+++ code/js/leic51n/browser-app-loading/s1.js
@@ -0,0 +1,7 @@
+import { add } from "./add.js";
+console.log("I'm s1");
+console.log(add(1, 2));
+const mainHeading = document.getElementById("main-heading");
+console.log(mainHeading);
+const something = "Hello";
+console.log(something);
---
+++ code/js/leic51n/browser-app-loading/s2.js
@@ -0,0 +1,5 @@
+import { add } from "./add.js";
+console.log("I'm s2");
+console.log(add(1, 2));
+const something = "world";
+console.log(something);
